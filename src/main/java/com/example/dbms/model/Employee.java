f11905fe18ffc614615fe41fbd71af462eb46038
Martys83 <aa7568719211@gmail.com>
2022-10-17 23:10:32 +0530

    first commit

---
+++ src/main/java/com/example/dbms/model/Employee.java
@@ -0,0 +1,116 @@
+package com.example.dbms.model;
+
+import java.util.Date;
+
+public class Employee {
+    private int eid;
+    private int aadhar_number;
+    private int salary;
+    private int account_no;
+    private int age;
+    private int phone_no;
+    private int pin;
+    private string ifsc;
+    private string dob;
+    private String first_name;
+    private String last_name;
+    private String Designation;
+    private string email;
+    private string city;
+    private string street;
+
+    public int getEid() {
+        return eid;
+    }
+    public void setEid(int eid) {
+        this.eid = eid;
+    }
+    public int getAadhar_number() {
+        return aadhar_number;
+    }
+    public void setAadhar_number(int aadhar_number) {
+        this.aadhar_number = aadhar_number;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    public int getAccount_no() {
+        return account_no;
+    }
+    public void setAccount_no(int account_no) {
+        this.account_no = account_no;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getPhone_no() {
+        return phone_no;
+    }
+    public void setPhone_no(int phone_no) {
+        this.phone_no = phone_no;
+    }
+    public int getPin() {
+        return pin;
+    }
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+    public string getIfsc() {
+        return ifsc;
+    }
+    public void setIfsc(string ifsc) {
+        this.ifsc = ifsc;
+    }
+    public string getDob() {
+        return dob;
+    }
+    public void setDob(string dob) {
+        this.dob = dob;
+    }
+    public String getFirst_name() {
+        return first_name;
+    }
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+    public String getLast_name() {
+        return last_name;
+    }
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+    public String getDesignation() {
+        return Designation;
+    }
+    public void setDesignation(String designation) {
+        Designation = designation;
+    }
+    public string getEmail() {
+        return email;
+    }
+    public void setEmail(string email) {
+        this.email = email;
+    }
+    public string getCity() {
+        return city;
+    }
+    public void setCity(string city) {
+        this.city = city;
+    }
+    public string getStreet() {
+        return street;
+    }
+    public void setStreet(string street) {
+        this.street = street;
+    }
+
+
+
+
+}
---
+++ src/main/java/com/example/dbms/model/Inventory.java
@@ -0,0 +1,5 @@
+package com.example.dbms.model;
+
+public class Inventory {
+    
+}
