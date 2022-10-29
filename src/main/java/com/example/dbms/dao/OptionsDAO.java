5ca6e9f27f0c00ec12159cf4c633484961709411
Alankrit <alankrit.kumar.cse20@itbhu.ac.in>
2022-10-29 11:40:21 +0530

    Questions and options

--- src/main/java/com/example/dbms/dao/OptionsDAO.java
+++ src/main/java/com/example/dbms/dao/OptionsDAO.java
@@ -1,5 +1,47 @@
 package com.example.dbms.dao;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.example.dbms.model.Options;
+import com.example.dbms.model.Student;
+
 public class OptionsDAO {
     
+@Autowired
+public JdbcTemplate temp;
+
+
+public void addOption(int option_id,int question_id, int text){
+    String sql="Insert into Options values(?,?,?);";
+    temp.update(sql,option_id,question_id,text);
+}
+
+public List<Options> getOptions(int question_id){
+    String sql="Select * from Options where Q_id = ? ;";
+    return temp.query(sql,new BeanPropertyRowMapper<>(Options.class),question_id);
+}
+
+public void deleteOption(int option_id,int question_id){
+    String sql="Delete from Options where Q_id = ? AND optionid=?;";
+    temp.update(sql,question_id,option_id);
+}
+
+public List<Student> getStudentsforoption(int option_id, int question_id){
+    String sql="Select * from Students where roll_no in(Select * from Opt_Student where optionid=? and Q_id=?)";
+
+    return temp.query(sql,new BeanPropertyRowMapper<>(Student.class),option_id,question_id);
+
+}
+
+public void chooseOption(int option_id,int question_id,int roll_no){
+    String sql="Insert into Opt_Student values(?,?,?);";
+    temp.update(sql,roll_no,question_id,option_id);
+
+}
+
+
 }
--- src/main/java/com/example/dbms/dao/QuestionsDAO.java
+++ src/main/java/com/example/dbms/dao/QuestionsDAO.java
@@ -1,5 +1,39 @@
 package com.example.dbms.dao;
 
+import java.sql.Time;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.example.dbms.model.Questions;
+
 public class QuestionsDAO {
+    @Autowired
+	private JdbcTemplate temp;
+
+
+    public void insertQuestion(int mess_id,int section_id,Time start_time, Time end_time,String text){
+        String sql = "Insert into Questions(mess_id,section_id,StarTime,EndTime,text) values(?,?,?,?,?)";
+        temp.update(sql,mess_id,section_id,start_time,end_time,text);
+    }
+    public List<Questions> getQuestions(){
+        String sql = "Select * from Questions";
+
+        return temp.query(sql,new BeanPropertyRowMapper<>(Questions.class));
+    }
+    public List<Questions> getQuestionbySection(int section_id){
+        String sql = "Select * from Questions where section_id = ?";
+
+        return temp.query(sql,new BeanPropertyRowMapper<>(Questions.class),section_id);
+    }
+    public Questions getQuestionbyId(int id){
+        String sql = "Select * from Questions where section_id = ?";
+
+        return temp.queryForObject(sql,new BeanPropertyRowMapper<>(Questions.class),id);
+
+    }
+    
     
 }
